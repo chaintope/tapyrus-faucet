230b1a6214f3b8eb18c219abc4cceb68b4d2cdf8
TORIFUKUKaiou <torifuku.kaiou@gmail.com>
2019-11-29 13:29:41 +0900

    Remove dependencies of monacon and koto
    Tapyrus is the greatest project and chain that I've ever met.
    This project supports only Tapyrus.
    I Was Born To Love Tapyrus.

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,3 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'monacoin/testnet/transactions#index'
-
-  get 'monacoin/testnet', to: 'monacoin/testnet/transactions#index'
-  get 'monacoin', to: 'monacoin/transactions#index'
-  get 'koto', to: 'koto/transactions#index'
-
-  namespace :monacoin do
-    resources :transactions, only: [:index, :create]
-
-    namespace :testnet do
-      resources :transactions, only: [:index, :create]
-    end
-  end
-
-  namespace :koto do
-    resources :transactions, only: [:index, :create]
-  end
 end
